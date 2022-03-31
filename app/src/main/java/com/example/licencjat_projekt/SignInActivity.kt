81252a754a34f24cc1316ed75de042b827924c79
mmotyl <mmotyl@sigma.ug.edu.pl>
2022-03-31 22:19:24 +0200

    added main, signup, signin

---
+++ app/src/main/java/com/example/licencjat_projekt/SignInActivity.kt
@@ -0,0 +1,30 @@
+package com.example.licencjat_projekt
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
+class SignInActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        signin_login.setOnClickListener(this)
+        signin_sign_up.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.signin_login -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.signin_sign_up -> {
+                val intent = Intent(this, SignUpActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/licencjat_projekt/SignUpActivity.kt
@@ -0,0 +1,30 @@
+package com.example.licencjat_projekt
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
+class SignUpActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        signup_sign_up.setOnClickListener(this)
+        signup_sign_in.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.signup_sign_up -> {
+                val intent = Intent(this, SignInActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.signup_sign_in -> {
+                val intent = Intent(this, SignInActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+}
