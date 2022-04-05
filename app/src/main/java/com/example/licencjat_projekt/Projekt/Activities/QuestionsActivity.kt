7d4d4b8e8a619c2a61fa05b03644698478f71dcd
mmotyl <mmotyl@sigma.ug.edu.pl>
2022-04-06 01:30:59 +0200

    repaired layouts, answer model for testing, scrolled answer view, alert dialog while answer/question del

--- app/src/main/java/com/example/licencjat_projekt/Projekt/Activities/QuestionsActivity.kt
+++ app/src/main/java/com/example/licencjat_projekt/Projekt/Activities/QuestionsActivity.kt
@@ -1,5 +1,6 @@
 package com.example.licencjat_projekt.Projekt.Activities
 
+import android.app.AlertDialog
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -27,7 +28,7 @@ class QuestionsActivity : AppCompatActivity(), View.OnClickListener{
             onBackPressed()
         }
         questions_add_button.setOnClickListener(this)
-        questions_delete_last.setOnClickListener(this)
+        questions_delete.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -41,23 +42,50 @@ class QuestionsActivity : AppCompatActivity(), View.OnClickListener{
                             Toast.LENGTH_SHORT
                         ).show()
                     }
-                    else -> { //TODO: testowo, poprawić później
+                    else -> { //TODO: repair adding models, just testing
                         val ans = AnswerModel(
                             questions_add_answer.text.toString()
                         )
+                        questions_add_answer.text.clear()
                         answersList.add(ans)
                         answersRecyclerView(answersList)
                     }
                 }
             }
-            R.id.questions_delete_last -> {
-                if(answersList.size > 0){
-                    answersList.removeLast()
-                    answersRecyclerView(answersList)
+            R.id.questions_delete -> {
+                val alert = AlertDialog.Builder(this)
+                alert.setTitle("Usuwanie:")
+                val items = arrayOf(
+                    "Pytanie",
+                    "Ostatnia odpowiedź"
+                )
+                alert.setItems(items) { _, n ->
+                    when (n) {
+                        0 -> removeQuestion()
+                        1 -> removeLastAnswer()
+                    }
                 }
+                alert.show()
             }
         }
     }
+    private fun removeQuestion(){
+        //TODO: remove question repair
+        questions_description.text.clear()
+        questions_add_answer.text.clear()
+        if(answersList.size > 0) {
+            answersList.clear()
+            answersRecyclerView(answersList)
+        }
+    }
+    private fun removeLastAnswer(){
+        //TODO: remove last answer repair
+        if(answersList.size > 0) {
+            answersList.removeLast()
+            answersRecyclerView(answersList)
+        }
+    }
+
     private fun answersRecyclerView(answers: ArrayList<AnswerModel>){
         questions_recycler_view.layoutManager = LinearLayoutManager(this)
         questions_recycler_view.setHasFixedSize(true)
@@ -65,10 +93,10 @@ class QuestionsActivity : AppCompatActivity(), View.OnClickListener{
         val answersList = AnswersList(this, answers)
         questions_recycler_view.adapter = answersList
 
-        /*answersList.setOnClickListener(object: AnswersList.OnClickListener{
+        answersList.setOnClickListener(object: AnswersList.OnClickListener{
             override fun onClick(position:Int, model: AnswerModel) {
-
+                Log.e("item", "${model.answer}")
             }
-        })*/
+        })
     }
 }
