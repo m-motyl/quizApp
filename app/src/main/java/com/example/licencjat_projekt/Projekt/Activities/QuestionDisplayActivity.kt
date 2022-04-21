af8cb27fc8db33fee569e63094ab5982ad39a108
Jan Dolkowski <jan.dolkowski@limebrains.com>
2022-04-21 11:08:35 +0200

    add visibility feature to question display

--- app/src/main/java/com/example/licencjat_projekt/Projekt/Activities/QuestionDisplayActivity.kt
+++ app/src/main/java/com/example/licencjat_projekt/Projekt/Activities/QuestionDisplayActivity.kt
@@ -1,8 +1,10 @@
 package com.example.licencjat_projekt.Projekt.Activities
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.licencjat_projekt.R
+import kotlinx.android.synthetic.main.activity_question_display.*
 import kotlinx.android.synthetic.main.activity_quiz_main.*
 
 class QuestionDisplayActivity : AppCompatActivity() {
@@ -13,5 +15,13 @@ class QuestionDisplayActivity : AppCompatActivity() {
         quizmain_toolbar.setNavigationOnClickListener{
             onBackPressed()
         }
+
+        if(null!=question_display_image.drawable)
+        {
+            question_display_image.visibility = View.VISIBLE
+        }else{
+            question_display_image.visibility = View.GONE
+        }
+
     }
 }
