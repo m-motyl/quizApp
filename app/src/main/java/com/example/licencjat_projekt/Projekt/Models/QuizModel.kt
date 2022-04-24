ecf689f7cca303e68f249c7d2a69b24d4bc74005
mmotyl <mmotyl@sigma.ug.edu.pl>
2022-04-24 18:39:58 +0200

    quizmodel

---
+++ app/src/main/java/com/example/licencjat_projekt/Projekt/Models/QuizModel.kt
@@ -0,0 +1,15 @@
+package com.example.licencjat_projekt.Projekt.Models
+
+data class QuizModel(
+    var title: String,
+    var time_limit: Int,
+    var description: String,
+    var gz_text: String,
+    var private: Boolean,
+    var invitation_code: String,
+    var correct_answers: Int,
+    var questions: Int,
+    var no_tries: Int,
+    var image: ByteArray,
+    var user: Int
+)
